92ebd9440c233f7fada7fec6359d329fd74811c1
Shameer Yusuff <yshameer@gmail.com>
2020-09-28 14:05:00 -0400

    Adding repository test

---
+++ src/test/java/com/example/demo/TodoRepositoryTest.java
@@ -0,0 +1,104 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.test.context.ActiveProfiles;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class TodoRepositoryTest {
+
+    @Autowired
+    TodoRepository todoRepository;
+    @Autowired
+    DatabaseClient database;
+
+    @BeforeEach
+    public void setUp() {
+
+        Hooks.onOperatorDebug();
+
+        List<String> statements = Arrays.asList(
+                "DROP TABLE IF EXISTS todo;",
+                "CREATE TABLE todo ( id SERIAL PRIMARY KEY, description VARCHAR(100) NOT NULL, details VARCHAR(100) NOT NULL, done BOOLEAN);");
+
+        statements.forEach(it -> database.execute(it)
+                .fetch()
+                .rowsUpdated()
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete());
+    }
+
+    @Test
+    public void test_Whether_FindAll_Returns_All_Rows_From_DB() {
+
+        Todo grocery=new Todo("Buy Milk","Buy Milk from store tomorrow",false);
+        Todo carwash=new Todo("Car Wash","Get car wash done today",false);
+
+        insertTodos(grocery, carwash);
+
+        todoRepository.findAll()
+                .as(StepVerifier::create)
+                .assertNext(grocery::equals)
+                .assertNext(carwash::equals)
+                .verifyComplete();
+    }
+
+    @Test
+    public void test_Whether_Save_Inserts_Data() {
+        Todo icecream=new Todo("Buy Icecream","Buy Icecream for kids today",false);
+        todoRepository.save(icecream)
+                .as(StepVerifier::create)
+                .expectNextMatches(todo -> todo.getId() != null)
+                .verifyComplete();
+    }
+
+    @Test
+    public void test_Whether_Delete_Removes_Data() {
+        Todo gas=new Todo("Fill Gas","Fill gas in jeep today",false);
+
+        Mono<Todo> deleted = todoRepository
+                .save(gas)
+                .flatMap(saved -> todoRepository.delete(saved).thenReturn(saved));
+
+        StepVerifier
+                .create(deleted)
+                .expectNextMatches(customer -> gas.getDescription().equalsIgnoreCase("Fill Gas"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void test_Whether_Update_Changes_Flag() {
+        Todo laundry=new Todo("Laundry","Do laundry today",false);
+
+        Mono<Todo> saved = todoRepository
+                .save(laundry)
+                .flatMap(todo -> {
+                    todo.setDone(true);
+                    return todoRepository.save(todo);
+                });
+
+        StepVerifier
+                .create(saved)
+                .expectNextMatches(Todo::isDone)
+                .verifyComplete();
+    }
+
+    private void insertTodos(Todo... todos) {
+
+        this.todoRepository.saveAll(Arrays.asList(todos))
+                .as(StepVerifier::create)
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+}
